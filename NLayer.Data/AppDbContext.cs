84a5944afa0ddfb0a7322819bf123784db8437c8
Besir Gunduz <besirgunduz1993@gmail.com>
2022-08-19 08:16:46 +0300

    AppDbContext

---
+++ NLayer.Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+
+namespace NLayer.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        //public DbSet<ProductFeature> ProductFeatures { get; set; }
+    }
+}
