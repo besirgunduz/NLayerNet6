c9813b57256e3d6d1f865e9296ebac72b0e7a8a1
Besir Gunduz <besirgunduz1993@gmail.com>
2022-08-20 11:26:18 +0300

    Seed Data

---
+++ NLayer.Data/Seeds/ProductFeatureSeed.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+
+namespace NLayer.Data.Seeds
+{
+    internal class ProductFeatureSeed : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasData(
+                new ProductFeature { Id = 1, Color = "Kırmızı", ProductId = 1 },
+                new ProductFeature { Id = 2, Color = "Mavi", ProductId = 2 });
+        }
+    }
+}
---
+++ NLayer.Data/Seeds/ProductSeed.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Data.Seeds
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product { Id = 1, CategoryId = 1, Name = "Kalem1", Price = 100, Stock = 10, CreatedDate = DateTime.Now },
+                new Product { Id = 2, CategoryId = 2, Name = "Silgi1", Price = 50, Stock = 150, CreatedDate = DateTime.Now });
+        }
+    }
+}
