3c6ec9cb953701b44d15b908e18878962b07dc11
Besir Gunduz <besirgunduz1993@gmail.com>
2022-08-20 11:26:03 +0300

    fix

--- NLayer.Core/BaseEntity.cs
+++ NLayer.Core/BaseEntity.cs
@@ -10,6 +10,6 @@ namespace NLayer.Core
     {
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; }
-        public DateTime UpdateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
     }
 }
